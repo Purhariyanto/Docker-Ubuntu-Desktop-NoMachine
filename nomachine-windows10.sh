590bcab3b3ec57aae1582f79b1b503841870a73d
Pur <purhariyanto1@gmail.com>
2023-08-11 15:16:16 +0700

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -35,7 +35,7 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=1 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
@@ -44,4 +44,4 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+seq 1 86400 | while read i; do echo -en "\r Running .     $i s /86400 s";sleep 0.1;echo -en "\r Running ..    $i s /86400 s";sleep 0.1;echo -en "\r Running ...   $i s /86400 s";sleep 0.1;echo -en "\r Running ....  $i s /86400 s";sleep 0.1;echo -en "\r Running ..... $i s /86400 s";sleep 0.1;echo -en "\r Running     . $i s /86400 s";sleep 0.1;echo -en "\r Running  .... $i s /86400 s";sleep 0.1;echo -en "\r Running   ... $i s /86400 s";sleep 0.1;echo -en "\r Running    .. $i s /86400 s";sleep 0.1;echo -en "\r Running     . $i s /86400 s";sleep 0.1; done
